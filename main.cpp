610aa4badbe5d1d6b5da33e8971099ea571f7d9f
Emerson Knapp <emerson@43layers.com>
2016-03-31 00:55:25 -0700

    Optimize and stretch image

--- main.cpp
+++ main.cpp
@@ -1,39 +1,64 @@
 #include "SDL.h"
 #include "SDL_image.h"
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+enum Assets {
+    ASSET_BACKGROUND,
+    ASSET_COUNT
+};
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gAssetSurfaces[ ASSET_COUNT ];
+
+
+
+
+
 void PrintSDLError(const char* const message) {
     printf("%s Error: %s\n", message, SDL_GetError());
 }
 
+SDL_Surface* LoadSurface( const char* path ) {
+    SDL_Surface* optimizedSurface = NULL;
+    SDL_Surface* loadedSurface = IMG_Load( path );
+    if ( loadedSurface == NULL ) {
+        PrintSDLError( "Couldn't load image" );
+    } else {
+        optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
+        if ( optimizedSurface == NULL ) {
+            PrintSDLError( "Unable to optimize surface" );
+        }
+        SDL_FreeSurface( loadedSurface );
+    }
+    return optimizedSurface;
+}
+
 int main( int /*argc*/, char** /*argv*/ ) {
-    SDL_Window * window;
-    SDL_Surface * screenSurface;
-    //Image to display on screen
-    SDL_Surface * helloWorld;
-    SDL_Event e;
+    // Initialization
     bool quit = false;
 
-    // Initialization
     if ( SDL_Init( SDL_INIT_VIDEO ) != 0 ) {
         PrintSDLError("SDL_Init");
         quit = true;
     } else {
-        window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-        if ( window == NULL ) {
+        gWindow = SDL_CreateWindow("Hello World!", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if ( gWindow == NULL ) {
             PrintSDLError("SDL_CreateWindow");
             quit = true;
         } else {
-            screenSurface = SDL_GetWindowSurface( window );
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
 
-            helloWorld = IMG_Load( "assets/01.jpg" );
-            if ( helloWorld == NULL ) {
-                PrintSDLError("Couldn't load image");
-                quit = true;
-            }
+            gAssetSurfaces[ ASSET_BACKGROUND ] = LoadSurface( "assets/02.png" );
         }
     }
 
     // Main Loop
+
+    SDL_Event e;
+
     while ( !quit ) {
         // Input handling
         while ( SDL_PollEvent( &e ) != 0 ) {
@@ -62,18 +87,22 @@ int main( int /*argc*/, char** /*argv*/ ) {
                 }
             }
         }
-        SDL_BlitSurface(helloWorld, NULL, screenSurface, NULL);
-        SDL_UpdateWindowSurface( window );
+
+        // Rendering
+        SDL_Rect stretchRect;
+        stretchRect.x = 0;
+        stretchRect.y = 0;
+        stretchRect.w = SCREEN_WIDTH;
+        stretchRect.h = SCREEN_HEIGHT;
+        SDL_BlitScaled( gAssetSurfaces[ ASSET_BACKGROUND ], NULL, gScreenSurface, &stretchRect );
+        //SDL_BlitSurface(gAssetSurfaces, NULL, screenSurface, NULL);
+        SDL_UpdateWindowSurface( gWindow );
     }
 
 
 
     // Shutdown
-    if ( helloWorld != NULL) {
-        SDL_FreeSurface(helloWorld);
-        helloWorld = NULL;
-    }
-    SDL_DestroyWindow( window );
+    SDL_DestroyWindow( gWindow );
     SDL_Quit();
     return 0;
 }
