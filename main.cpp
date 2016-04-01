8f0f6d7f21acf5e463449c2e4edcf8e7ac8b8ef1
Emerson Knapp <emerson@43layers.com>
2016-04-01 01:17:25 -0700

    Start making blocks? Draw one

--- main.cpp
+++ main.cpp
@@ -7,26 +7,20 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 const int PLAYAREA_WIDTH = 10;
-const int PLAYAREA_HEIGHT = 15;
-
-typedef struct QuadBlock {
-    int x = 0;
-    int y = 0;
-} QuadBlock;
-
-typedef struct GameState {
-    float fallsPerSecond = 1;
-    std::vector<QuadBlock*> blocks;
-} GameState;
+const int PLAYAREA_HEIGHT = 10;
 
 void PrintSDLError( const char* message ) {
     printf("%s Error: %s\n", message, SDL_GetError());
 }
 
-typedef struct Platform {
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
-} Platform;
+SDL_Rect Rect( int x, int y, int w, int h ) {
+    SDL_Rect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = w;
+    rect.h = h;
+    return rect;
+}
 
 SDL_Texture* loadTexture( const char* path, SDL_Renderer* renderer ) {
     SDL_Texture* newTexture = NULL;
@@ -42,6 +36,23 @@ SDL_Texture* loadTexture( const char* path, SDL_Renderer* renderer ) {
     }
     return newTexture;
 }
+
+struct QuadBlock {
+    int x;
+    int y;
+};
+
+typedef struct GameState {
+    float fallsPerSecond = 1;
+    std::vector<QuadBlock> blocks;
+} GameState;
+
+typedef struct Platform {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+} Platform;
+
+
 
 bool loadMedia(SDL_Renderer* /*renderer*/) {
     bool success = true;
@@ -107,24 +118,17 @@ void shutdownSDL( Platform* platform ) {
     platform->window = NULL;
     IMG_Quit();
     SDL_Quit();
+
+    delete platform;
 }
 
-SDL_Rect Rect( int x, int y, int w, int h ) {
-    SDL_Rect rect;
-    rect.x = x;
-    rect.y = y;
-    rect.w = w;
-    rect.h = h;
-    return rect;
-}
-
-void drawGame( SDL_Renderer* renderer ) {
+void drawGame( SDL_Renderer* renderer, const GameState* gameState ) {
     // background
     SDL_SetRenderDrawColor( renderer, 0xFF, 0x00, 0x00, 0xFF );
     SDL_RenderClear( renderer );
 
     // Game area
-    int gameAreaWidth = 2 * SCREEN_WIDTH / 3;
+    int gameAreaWidth = 3 * SCREEN_WIDTH / 4;
     int gameAreaHeight = SCREEN_HEIGHT;
     SDL_Rect gameAreaViewport = Rect( 0, 0, gameAreaWidth, gameAreaHeight );
     SDL_RenderSetViewport( renderer, &gameAreaViewport );
@@ -132,12 +136,26 @@ void drawGame( SDL_Renderer* renderer ) {
     SDL_Rect gameAreaBackground = Rect( 0, 0, gameAreaWidth, gameAreaHeight );
     SDL_RenderFillRect( renderer, &gameAreaBackground );
 
+    // Blocks
+    int blockWidth = gameAreaWidth / PLAYAREA_WIDTH;
+    int blockHeight = gameAreaHeight / PLAYAREA_HEIGHT;
+    SDL_SetRenderDrawColor( renderer, 0x00, 0xAA, 0xAA, 0xFF );
+    SDL_Rect blockRect;
+    for ( const QuadBlock& qb : gameState->blocks ) {
+        blockRect = Rect( qb.x * blockWidth, qb.y * blockHeight, blockWidth, blockHeight );
+        SDL_RenderFillRect( renderer, &blockRect );
+    }
+
     SDL_RenderPresent( renderer );
 }
 
 void mainLoop( SDL_Renderer* renderer ) {
     bool quit = false;
     SDL_Event e;
+
+    GameState gameState;
+    QuadBlock block0 = { 0, 0 };
+    gameState.blocks.push_back( block0 );
 
     while ( !quit ) {
         // Input handling
@@ -168,9 +186,11 @@ void mainLoop( SDL_Renderer* renderer ) {
             }
         }
 
-        drawGame( renderer );
+        drawGame( renderer, &gameState );
     }
-    return;
+
+
+
 }
 
 int main( int /*argc*/, char** /*argv*/ ) {
